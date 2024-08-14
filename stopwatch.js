0ab2499633f23ce4bacb25179bed4fddee877f7c
{klaudiasobanska} <{klaudia.sobanska@outlook.com}>
2024-08-14 11:14:57 +0200

    buttons effect & stop button removed

--- stopwatch.js
+++ stopwatch.js
@@ -2,37 +2,49 @@ const minutesText = document.getElementById("minutes");
 const secondsText = document.getElementById("seconds");
 const centisecondsText = document.getElementById("centiseconds");
 const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
 const restartButton = document.getElementById("restart");
+const buttons = document.getElementsByClassName("button--hover");
 
 let time = 0;
 let isRunning = false;
 let timeInterval;
 
 startButton.addEventListener("click", () => {
-  isRunning = true;
+  isRunning = !isRunning;
   if (!timeInterval) {
     timeInterval = setInterval(setTime, 10);
   }
-  startButton.classList.add("button--active");
-});
-
-stopButton.addEventListener("click", () => {
-  isRunning = false;
-  startButton.classList.remove("button--active");
 });
 
 restartButton.addEventListener("click", () => {
   clearInterval(timeInterval);
   timeInterval = null;
   isRunning = false;
+  changeStartButton();
   time = 0;
   minutesText.textContent = "00";
   secondsText.textContent = "00";
   centisecondsText.textContent = "00";
 });
 
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("mousemove", (e) => {
+    let x = e.pageX - button.offsetLeft;
+    let y = e.pageY - button.offsetTop;
+
+    if (button.className.includes("button--active")) {
+      button.style.setProperty("--bgcolor", "#d479ca");
+    } else {
+      button.style.setProperty("--bgcolor", "#faa063");
+    }
+
+    button.style.setProperty("--x", x + "px");
+    button.style.setProperty("--y", y + "px");
+  });
+});
+
 function setTime() {
+  changeStartButton();
   if (!isRunning) return;
   time++;
   let centiseconds = time % 100;
@@ -50,4 +62,15 @@ function setTime() {
   minutesText.textContent = minutes;
   secondsText.textContent = seconds;
   centisecondsText.textContent = centiseconds;
+}
+
+function changeStartButton() {
+  if (isRunning) {
+    startButton.classList.add("button--active");
+    startButton.children[0].innerHTML = "Stop";
+  } else {
+    startButton.classList.remove("button--active");
+    startButton.children[0].innerHTML = "Start";
+  }
+  
 }
